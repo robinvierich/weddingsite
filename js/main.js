91bd2c51e8033151e9b3c451ac28f365fd304688
robin <robinvierich@gmail.com>
2017-02-28 18:09:33 -0500

    updated main.js to include correct css file
    Silly/slow jquery scroll to anchor code (probably good enough - we'll see)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-require("styles/main.scss");
+require("styles/include.scss");
 
 require("script-loader!js/vendor/jquery.js");
 require("script-loader!js/vendor/what-input.js");
 require("script-loader!js/vendor/foundation.js");
 
 require ('index.html');
+
+
+$(document).ready(function() {
+    $(document).foundation();
+
+    $('a[href^="#"]').click(function(e) { 
+        e.preventDefault(); 
+
+        var dest = $(this).attr('href'); 
+        console.log(dest); 
+        $('html,body').animate({ scrollTop: $(dest).position().top + 1 }, 'slow');
+    });
+});
+    
